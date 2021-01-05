3a73f060ccc3677038a5c4d1a1228636bb1abbcc
Raphael <raphael.lukas1983@gmail.com>
2021-01-05 11:24:33 +0100

    Chapter 6 done

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public List<Item> Items { get; set; }
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            Items = _context.Items.ToList();
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Creat()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item item = _context.Items.FirstOrDefault(item => item.Id == Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
